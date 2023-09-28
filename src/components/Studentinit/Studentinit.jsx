828629a049ac70438b2faad81779bf7b2d4c1543
StivenCarvajalCampus <stivencarvajal3211@gmail.com>
2023-09-28 09:14:35 -0500

    feat(Students): :sparkles:

---
+++ src/components/Studentinit/Studentinit.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+  Card,
+  CardBody,
+  CardHeader,
+  Image,
+} from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+import { useLocation } from "react-router-dom";
+
+export default function Dashboard() {
+  const location = useLocation();
+  const userId = new URLSearchParams(location.search).get("userId");
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    // Realizar una solicitud para obtener los datos del usuario
+    if (userId) {
+      fetch(`http://127.25.25.26:3302/user/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setUserData(data); // Guardar los datos del usuario en el estado
+        })
+        .catch((error) => {
+          console.error("Error al obtener los datos del usuario: ", error);
+        });
+    }
+  }, [userId]);
+
+  return (
+    <div className="w-full">
+      <Navbar className="bg-white">
+        <Navbar className="bg-white">
+          <NavbarBrand>
+            <AcmeLogo />
+            <p className="font-bold text-inherit">Skillup</p>
+          </NavbarBrand>
+
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Features
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page" color="secondary">
+                Customers
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Integrations
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+
+          <NavbarContent as="div" justify="end">
+            <Dropdown placement="bottom-end">
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as="button"
+                  className="transition-transform"
+                  color="secondary"
+                  name="Jason Hughes"
+                  size="sm"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                />
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Profile Actions" variant="flat">
+                <DropdownItem key="profile" className="h-14 gap-2">
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-semibold">{userData.username}</p>
+                </DropdownItem>
+                <DropdownItem key="settings">My Settings</DropdownItem>
+                <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                <DropdownItem key="analytics">Analytics</DropdownItem>
+                <DropdownItem key="system">System</DropdownItem>
+                <DropdownItem key="configurations">Configurations</DropdownItem>
+                <DropdownItem key="help_and_feedback">
+                  Help & Feedback
+                </DropdownItem>
+                <DropdownItem key="logout" color="danger">
+                  Log Out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarContent>
+        </Navbar>
+      </Navbar>
+
+      <div className="mt-4 mx-auto max-w-md">
+        <Card className="py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold">Cantidad de videos </p>
+            <small className="text-default-500">12 Tracks</small>
+            <h4 className="font-bold text-large">React</h4>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2">
+            <Image
+              alt="Card background"
+              className="object-cover rounded-xl"
+              src="src/assets/images/react.jpg"
+              width={270}
+            />
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
+}
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./dashboard/Dashboard";
 import App from "./login/App";
+import Studentinit from "./Studentinit/Studentinit"
+import { element } from "prop-types";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <Dashboard />,
+  },
+  {
+    path: "/studentinit",
+    element: <Studentinit />,
   },
 ]);
 export default router;
