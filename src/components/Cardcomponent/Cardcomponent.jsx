b7338e503cb610e7a1d1959d5f1c213b914d1141
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-09-29 01:10:00 -0500

    feat(StudentInit): :sparkles: Screen StudentInit
    full student home screen

--- src/components/Cardcomponent/Cardcomponent.jsx
+++ src/components/Cardcomponent/Cardcomponent.jsx
@@ -1,22 +1,21 @@
-// CardComponent.js
-import React from 'react';
-import {
-    Card,
-    CardBody,
-    CardHeader,
-    Image,
-  }from "@nextui-org/react";
+import React from "react";
+import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 
-function CardComponent({ title,subdescription, description, imageSrc }) {
+function CardComponent({ title, subdescription, description, imageSrc,Trainer }) {
   return (
-    <div className="mt-4 mx-auto max-w-md m-4">
-      <Card className="py-4">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start m-4">
-          <p className="text-tiny uppercase font-bold">
-            {title}
-          </p>
-          <small className="text-default-500">{subdescription}</small>
-          <h4 className="font-bold text-large">{description}</h4>
+    <div className="flex-wrap max-w-md m-4">
+      {" "}
+      {/* Añadimos margen aquí */}
+      <Card
+        isBlurred
+        className="py-4 px-2 bg-background/10 dark:bg-default-100/50 max-w-[610px]  "
+        shadow="sm"
+      >
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold text-white">{title}</p>
+          <small className="text-default-500 text-white">{subdescription}</small>
+          <h4 className="font-bold text-large text-white">{description}</h4>
+          <small className="text-default-500 text-white">{Trainer}</small>
         </CardHeader>
         <CardBody className="overflow-visible py-2">
           <Image
@@ -24,6 +23,7 @@ function CardComponent({ title,subdescription, description, imageSrc }) {
             className="object-cover rounded-xl"
             src={imageSrc}
             width={270}
+            onClick="Hola"
           />
         </CardBody>
       </Card>
--- src/components/Studentinit/Studentinit.jsx
+++ src/components/Studentinit/Studentinit.jsx
@@ -1,47 +1,54 @@
-  import React, { useEffect, useState } from "react";
-  import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Link,
-    DropdownItem,
-    DropdownTrigger,
-    Dropdown,
-    DropdownMenu,
-    Avatar,
-    Card,
-    CardBody,
-    CardHeader,
-    Image,
-  } from "@nextui-org/react";
-  import { useLocation } from "react-router-dom";
-  import CardComponent from "../Cardcomponent/Cardcomponent.jsx";
-  import NavbarComponent from "../NavbarComponent/NavbarComponent.jsx";
+import React from "react";
+import CardComponent from "../Cardcomponent/Cardcomponent.jsx";
+import NavbarComponent from "../NavbarComponent/NavbarComponent.jsx";
 
-  export default function Dashboard() {
-   
-
-    return (
-      <div className="w-full">
-        <NavbarComponent/>
-
-        <div className="flex flex-wrap ">
-          
-          <CardComponent 
-          title="Cantidad de videos"
-          subdescription="12 Tracks"
-          description="React"
-          imageSrc="src/assets/images/react.jpg"
-          />
+export default function Dashboard() {
+  return (
+    <div className="w-full">
+      <NavbarComponent />
+      <div className="flex flex-row -m-2 justify-center">
+        <div className="flex flex-row flex-col mt-4 mx-auto max-w-md m-4 p-4 ">
           <CardComponent
-          title="Cantidad de videos"
-          subdescription="12 Tracks"
-          description="React"
-          imageSrc="src/assets/images/git.jpg"
+            title="Cantidad de videos"
+            subdescription="12 Tracks"
+            description="React"
+            imageSrc="src/assets/images/react.jpg"
+            Trainer="Victor Robles "
+            style={{ margin: "0.5rem" }}
           />
-          
+          <br />
+
+          <CardComponent
+            title="Cantidad de videos"
+            subdescription="12 Tracks"
+            description="Git"
+            imageSrc="src/assets/images/git.jpg"
+            Trainer="FapCodYT"
+            style={{ margin: "0.5rem" }}
+          />
+        </div>
+
+        <div className="flex flex-col  mt-4 mx-auto max-w-md m-10 p-4 ">
+          <CardComponent
+            title="Cantidad de videos"
+            subdescription="12 Tracks"
+            description="Java Script"
+            imageSrc="src/assets/images/js.jpg"
+            style={{ margin: "0.5rem" }}
+            Trainer="Victor Robles"
+            className="p-4"
+          />
+          <br></br>
+          <CardComponent
+            title="Cantidad de videos"
+            subdescription="12 Tracks"
+            description="React desde cero"
+            imageSrc="src/assets/images/reactdecero.jpg"
+            Trainer="Fernando Herrera"
+            style={{ margin: "0.5rem" }}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
